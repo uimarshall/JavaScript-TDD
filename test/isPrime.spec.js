3ac38d7cbd6ade3b1afb7487b4881ae3e77604db
uimarshall <uimarshall@gmail.com>
2019-07-19 15:18:23 +0100

    modified isprime function

--- test/isPrime.spec.js
+++ test/isPrime.spec.js
@@ -1,7 +1,7 @@
-const { isPrime } = require("../integrationTest/isPrime");
+const isPrime = require("../integrationTest/isPrime");
 test("2 should return true as prime", () => {
 	const actual = isPrime(2);
-	expect(actual).toBeTruthy;
+	expect(actual).toBe(true);
 });
 test("10 should return false as not prime", () => {
 	const actual = isPrime(10);
