983bd71b0b5b0962837edbdded1970a076bfe5de
uimarshall <uimarshall@gmail.com>
2019-07-20 13:50:45 +0100

    modified connection function

--- NodeJsTest/test/connection.js
+++ NodeJsTest/test/connection.js
@@ -1,12 +1,11 @@
-
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 // Connect to the db b4 tests run
-before(function(done) {
+before(done => {
 	// connect to db
-	mongoose.connect("mongodb://localhost/testdb");
+	mongoose.connect("mongodb://localhost/testdb", { useNewUrlParser: true });
 	// listen to when the connection is successfully made
-	
+
 	mongoose.connection
 		.once("open", function() {
 			console.log("Connection made");
@@ -17,8 +16,7 @@ before(function(done) {
 		});
 });
 
-
-beforeEach(function(done) {
+beforeEach(done => {
 	// Drop the coll
 	mongoose.connection.collections.clients.drop(function() {
 		done();
